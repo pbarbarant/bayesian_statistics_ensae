f734de0c49bcd95b8c64bb031605e954c0ce650c
Pierre-Louis Barbarant <plbarbarant@gmail.com>
2024-01-21 19:00:10 +0100

    Add Gibbs sampler trace plot and save coefficients to CSV

--- final_project.py
+++ final_project.py
@@ -1,4 +1,5 @@
 # %%
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy import stats
@@ -49,6 +50,7 @@ def gibbs_sampling(X, y, N_iter):
     """Run the Gibbs sampler for N_iter iterations"""
     # Initialize parameters
     q_chain = np.zeros(N_iter)
+    beta_chain = np.zeros((N_iter, X.shape[1]))
     y_pred_chain = np.zeros((N_iter, y.shape[0]))
     beta, sigma2, R2, q, z, k, T = init_params(X, y)
 
@@ -62,11 +64,12 @@ def gibbs_sampling(X, y, N_iter):
         )
         q_chain[i] = q
         y_pred_chain[i] = (X @ beta).reshape(-1)
-    return q_chain, y_pred_chain, beta
+        beta_chain[i] = beta.reshape(-1)
+    return q_chain, y_pred_chain, beta_chain
 
 
 if __name__ == "__main__":
-    N_iter, burn_in = 11_000, 1000
+    N_iter, burn_in = 1100, 100
 
     df, timestamp = preprocess_dataset()
     X = df.iloc[1:, :-1].values
@@ -78,39 +81,65 @@ if __name__ == "__main__":
     y = scaler.fit_transform(y)
 
     # Run Gibbs sampler
-    q_chain, y_pred, beta = gibbs_sampling(X, y, N_iter)
+    q_chain, y_pred_chain, beta_chain = gibbs_sampling(X, y, N_iter)
     # Remove burn-in
     q_chain = q_chain[burn_in:]
-    y_pred = y_pred[burn_in:]
+    y_pred_chain = y_pred_chain[burn_in:]
+    beta_chain = beta_chain[burn_in:]
 
-    # Get credible regions
-    cr = credible_regions(y_pred)
-
-    # Rescale y, y_pred, and cr
-    cr = scaler.inverse_transform(cr)
-    y = scaler.inverse_transform(y)
-    y_pred = scaler.inverse_transform(y_pred)
+    # q_chain trace plot
+    fig = plt.figure(figsize=(10, 5))
+    plt.plot(q_chain, color="black")
+    plt.xlabel("Iteration")
+    plt.ylabel("q")
+    plt.title("Trace plot of q")
+    plt.savefig("figures/q_chain_trace.png", bbox_inches="tight", dpi=300)
 
     df_gibbs = pd.DataFrame(
         {
             "Variable": df.columns[:-1],
-            "Coefficient": beta.flatten(),
+            "Mean Posterior Coefficient": np.mean(beta_chain, axis=0),
+            "Standard Error": np.std(beta_chain, axis=0),
+            "5% lower bound": np.quantile(beta_chain, 0.025, axis=0),
+            "95% upper bound": np.quantile(beta_chain, 0.975, axis=0),
+            "Posterior probability of inclusion": np.mean(
+                beta_chain != 0, axis=0
+            ),
         },
         index=df.columns[:-1],
     )
-    # Save non-zero coefficients to csv with no index
-    df_gibbs[df_gibbs["Coefficient"] != 0].to_csv(
-        "coefficients/gibbs.csv", index=False
+    # Sort coefficients by posterior probability of inclusion
+    df_gibbs = df_gibbs.sort_values(
+        "Posterior probability of inclusion", ascending=False
     )
+    # Remove coefficients with posterior probability of inclusion < median of q_chain
+    df_gibbs = df_gibbs[df_gibbs["Posterior probability of inclusion"] > 0.5]
+    # Set df_gibbs precision to 4 decimal places
+    df_gibbs = df_gibbs.round(4)
+    # Save non-zero coefficients to csv
+    df_gibbs.to_csv("coefficients/gibbs.csv", index=False)
+
+    # Get credible regions
+    cr = credible_regions(y_pred_chain)
+
+    # Rescale y, y_pred_chain, and cr
+    cr = scaler.inverse_transform(cr)
+    y = scaler.inverse_transform(y)
+    y_pred_chain = scaler.inverse_transform(y_pred_chain)
+
+    # Calculate R2
+    y_pred = y_pred_chain.mean(axis=0)
+    correlation_matrix = np.corrcoef(y.reshape(-1), y_pred)
+    R2 = correlation_matrix[0, 1] ** 2
 
     # Plot actual vs. predicted values
     plot_INDPRO_RATE(
         y,
-        y_pred.mean(axis=0),
+        y_pred,
         cr,
         timestamp,
         regions="cr",
-        title="Rate of Change in Industrial Production Index (INDPRO)\n Actual vs. Predicted Values using Bayesian Regression",
+        title=f"Rate of Change in Industrial Production Index (INDPRO)\n Actual vs. Predicted Values using Bayesian Regression, R2 = {R2:.4f}",
     )
 
     # Plot posterior distribution of q
